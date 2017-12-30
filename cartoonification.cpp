16b8ef8c2fc6a8e08606e40eba6d45c56ed0df52
kushashwa ravi shrimali <kushashwaravishrimali@gmail.com>
2017-12-31 00:15:46 +0530

    Added: Showing face layout

--- cartoonification.cpp
+++ cartoonification.cpp
@@ -14,6 +14,7 @@ using namespace cv;
 // Function declaration
 void cartoonifyImage(Mat srcColor, Mat dst, int);
 // void evilify(Mat src, Mat dst);
+void draw_face(Mat dst);
 
 int main(int argc, char** argv) {
     int cameraNo = 0;
@@ -43,6 +44,7 @@ int main(int argc, char** argv) {
         Mat displayedFrame(cameraFrame.size(), CV_8UC3); // 8 bit unsigned, 3 channels
         // Mat newdisplayFrame(cameraFrame.size(), CV_8UC3);
         cartoonifyImage(cameraFrame, displayedFrame, 0);
+        draw_face(displayedFrame);
         // evilify(cameraFrame, newdisplayFrame);
 
         // imshow("Evilifier", newdisplayFrame);
@@ -52,7 +54,54 @@ int main(int argc, char** argv) {
         if(keyPress == 27) { break; } // esc
     } // end while
 }
+// Showing user where to put face
+void draw_face(Mat dst) {
+    // ellipse covering 70 percent of img height
+    // fixed aspect ratio of 0.72 - face will not become too skinny or fat
 
+    // Draw color face on a black background
+    Size size = dst.size();
+    Mat faceOutline = Mat::zeros(size, CV_8UC3);
+    Scalar color = CV_RGB(255, 255, 0); // YELLOW 
+    int thickness = 4;
+
+    int sh = size.height;
+    int sw = size.width;
+
+    int faceH = sh/2 * 70/100; // faceH - radius of ellipse, half of 0.7*sh
+    int faceW = faceH * 72/100;
+
+    // Draw face outline
+    ellipse(faceOutline, Point(sw/2, sh/2), Size(faceW, faceH), 0, 0, 360, color, thickness, CV_AA);
+    cout << "Scalar color is: " << color;
+
+    // 2 eye arcs 
+    int eyeW = faceW * 23/100;
+    int eyeH = faceH * 11/100;
+    int eyeX = faceW * 48/100;
+    int eyeY = faceH * 13/100;
+
+    Size eyeSize = Size(eyeW, eyeH);
+    // Set angle and shift for eye half ellipses
+    int eyeA = 15; // angle in degrees
+    int eyeYshift = 11;
+
+    // draw top of right eye
+    ellipse(faceOutline, Point(sw/2 - eyeX, sh/2 - eyeY), eyeSize, 0, 180+eyeA, 360-eyeA, color 
+    , thickness, CV_AA);
+    // draw bottom of right eye
+    ellipse(faceOutline, Point(sw/2 - eyeX, sh/2 - eyeY - eyeYshift), eyeSize, 0, 0+eyeA, 180-eyeA, color 
+    , thickness, CV_AA);
+    // draw top of left eye
+    ellipse(faceOutline, Point(sw/2 + eyeX, sh/2 - eyeY), eyeSize, 0, 180+eyeA, 360-eyeA, color 
+    , thickness, CV_AA);
+    // draw bottom of left eye
+    ellipse(faceOutline, Point(sw/2 + eyeX, sh/2 - eyeY - eyeYshift), eyeSize, 0, 0+eyeA, 180-eyeA, color 
+    , thickness, CV_AA);
+    
+    // overlay with alpha value
+    addWeighted(dst, 1.0, faceOutline, 0.7, 0, dst, CV_8UC3);
+}
 void cartoonifyImage(Mat srcColor, Mat dst, int evilify) {
     Mat gray; // make gray image - blank
     cvtColor(srcColor, gray, CV_BGR2GRAY);
@@ -98,7 +147,7 @@ void cartoonifyImage(Mat srcColor, Mat dst, int evilify) {
     int repetitions = 7;
 
     for(int i = 0; i<repetitions; i++) {
-        int ksize=9;
+        int ksize=21;
         double sigmaColor = 9;
         double sigmaSpace = 7;
         bilateralFilter(smallImg, tmp, ksize, sigmaColor, sigmaSpace);
