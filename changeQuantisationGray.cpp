1653c4bff3baa60cefd20210bd028c3376813ab8
kushashwa ravi shrimali <kushashwaravishrimali@gmail.com>
2017-12-31 23:33:04 +0530

    Adding change Quantisation code

---
+++ changeQuantisationGray.cpp
@@ -0,0 +1,62 @@
+#include <opencv2/opencv.hpp>
+#include "highgui.h"
+// #include "imgproc.h"
+// #include  "/usr/include/opencv2/imgproc/imgproc.hpp"
+
+#include  <iostream>
+// #include  <iomanip>
+#include <cstdlib>
+
+using namespace std;
+using namespace cv;
+
+// Function declaration
+void cartoonifyImage(Mat, Mat, int, int);
+// void evilify(Mat src, Mat dst);
+void removePepperNoise(Mat &mask);
+void draw_face(Mat dst);
+
+int main(int argc, char** argv) {
+    int cameraNo = 0;
+    if(argc > 1)
+        cameraNo = atoi(argv[1]); // convert character to int for cameraNo
+    // get access to camera
+    
+    VideoCapture camera;
+    camera.open(cameraNo);
+
+    if(!camera.isOpened()) {
+        cerr << "Error: Could not access camera/video" << endl;
+        exit(1);
+    }
+    camera.set(CV_CAP_PROP_FRAME_WIDTH, 640);
+    camera.set(CV_CAP_PROP_FRAME_WIDTH, 480);
+
+    while(true) {
+        // grab next camera 
+        Mat cameraFrame;
+        camera >> cameraFrame;
+        if(cameraFrame.empty()) {
+            cerr << "ERROR: Couldn't grab a camera frame." << endl;
+            exit(1);
+        }
+        // create blank image that we will draw into
+        imshow("original", cameraFrame);
+        changeQuantisationGray(cameraFrame, 8);
+        imshow("New", cameraFrame);
+
+        char keyPress = waitKey(20);
+        if(keyPress == 27) break;
+    }
+}
+
+void changeQuantisationGray(Mat cameraFrame, int num_bits) {
+    assert( (image.type() == CV_8UC1) && (num_bits >= 1) && (num_bits <= 8) );
+    uchar mask = 0xFF << (8 - num_bits);
+    for(int row = 0; row < image.rows; row++) {
+        for(int col = 0; col < image.cols; col++) {
+            image.at<uchar>(row, col) = 
+                                        image.at<uchar>(row, col) & mask;
+        }   
+    }
+}
