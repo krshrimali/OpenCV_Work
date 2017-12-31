f7d61ecb70f457274d7b01a70216191414ad4aea
kushashwa ravi shrimali <kushashwaravishrimali@gmail.com>
2017-12-31 10:03:11 +0530

    Added removePepperNoise.

--- cartoonification.cpp
+++ cartoonification.cpp
@@ -12,6 +12,7 @@ using namespace cv;
 // Function declaration
 void cartoonifyImage(Mat, Mat, int, int);
 // void evilify(Mat src, Mat dst);
+void removePepperNoise(Mat &mask);
 void draw_face(Mat dst);
 
 int main(int argc, char** argv) {
@@ -133,19 +134,20 @@ void cartoonifyImage(Mat srcColor, Mat dst, int evilify, int alienMode) {
         edges += edges2;
         const int EVIL_EDGE_THRESHOLD = 12;
         threshold(edges, masking, EVIL_EDGE_THRESHOLD, 255, THRESH_BINARY_INV);
-        imshow("edges", edges);
+        // imshow("edges", edges);
         medianBlur(masking, masking, 3);
-        imshow("masking", masking);
+        // imshow("masking", masking);
         // dst = masking;
         // dst.copyTo(dst, masking);  
     } else {
         const int LAPLACIAN_FILTER_SIZE = 5;
         Laplacian(gray, edges, CV_8U, LAPLACIAN_FILTER_SIZE);
-        imshow("edges_2", edges);
+        // imshow("edges_2", edges);
         // Mat masking;
         const int EDGES_THRESHOLD = 80;
         threshold(edges, masking, EDGES_THRESHOLD, 255, THRESH_BINARY_INV);
-        imshow("edges_3", masking);
+        removePepperNoise(masking);
+        // imshow("edges_3", masking);
     }
     // reduce pixels size by quarter - to make acceptable speed for bilateral filter
     Size smallSize;
@@ -218,7 +220,7 @@ void cartoonifyImage(Mat srcColor, Mat dst, int evilify, int alienMode) {
         int Blue = 0;
         add(smallImg, Scalar(Blue, Green, Red), smallImg, mask);
 
-        imshow("smallImg", smallImg);
+        // imshow("smallImg", smallImg);
         // imshow("yuv", edgeMask);
     }
     // Mat bigImg = Mat(size, CV_8U);
@@ -228,4 +230,62 @@ void cartoonifyImage(Mat srcColor, Mat dst, int evilify, int alienMode) {
     srcColor.copyTo(dst, masking);
     // return dst;
     // return mask_;
+}
+
+void removePepperNoise(Mat &mask)
+{
+    // For simplicity, ignore the top & bottom row border.
+    for (int y=2; y<mask.rows-2; y++) {
+        // Get access to each of the 5 rows near this pixel.
+        uchar *pThis = mask.ptr(y);
+        uchar *pUp1 = mask.ptr(y-1);
+        uchar *pUp2 = mask.ptr(y-2);
+        uchar *pDown1 = mask.ptr(y+1);
+        uchar *pDown2 = mask.ptr(y+2);
+
+        // For simplicity, ignore the left & right row border.
+        pThis += 2;
+        pUp1 += 2;
+        pUp2 += 2;
+        pDown1 += 2;
+        pDown2 += 2;
+        for (int x=2; x<mask.cols-2; x++) {
+            uchar v = *pThis;   // Get the current pixel value (either 0 or 255).
+            // If the current pixel is black, but all the pixels on the 2-pixel-radius-border are white
+            // (ie: it is a small island of black pixels, surrounded by white), then delete that island.
+            if (v == 0) {
+                bool allAbove = *(pUp2 - 2) && *(pUp2 - 1) && *(pUp2) && *(pUp2 + 1) && *(pUp2 + 2);
+                bool allLeft = *(pUp1 - 2) && *(pThis - 2) && *(pDown1 - 2);
+                bool allBelow = *(pDown2 - 2) && *(pDown2 - 1) && *(pDown2) && *(pDown2 + 1) && *(pDown2 + 2);
+                bool allRight = *(pUp1 + 2) && *(pThis + 2) && *(pDown1 + 2);
+                bool surroundings = allAbove && allLeft && allBelow && allRight;
+                if (surroundings == true) {
+                    // Fill the whole 5x5 block as white. Since we know the 5x5 borders
+                    // are already white, just need to fill the 3x3 inner region.
+                    *(pUp1 - 1) = 255;
+                    *(pUp1 + 0) = 255;
+                    *(pUp1 + 1) = 255;
+                    *(pThis - 1) = 255;
+                    *(pThis + 0) = 255;
+                    *(pThis + 1) = 255;
+                    *(pDown1 - 1) = 255;
+                    *(pDown1 + 0) = 255;
+                    *(pDown1 + 1) = 255;
+                }
+                // Since we just covered the whole 5x5 block with white, we know the next 2 pixels
+                // won't be black, so skip the next 2 pixels on the right.
+                pThis += 2;
+                pUp1 += 2;
+                pUp2 += 2;
+                pDown1 += 2;
+                pDown2 += 2;
+            }
+            // Move to the next pixel.
+            pThis++;
+            pUp1++;
+            pUp2++;
+            pDown1++;
+            pDown2++;
+        }
+    }
 }
