cbe969fa87ef777a72af61b85a5e2b2a0461aaaf
kushashwa ravi shrimali <kushashwaravishrimali@gmail.com>
2018-01-10 07:31:10 +0530

    removed unwanted files

--- blurring_source.cpp
+++
@@ -1,6 +0,0 @@
-#include <iostream>
-
-void cv::blur( InputArray src, OutputArray dst, Size ksize, Point anchor, int borderType) {
-    CV_INSTRUMENT_REGION();
-    boxFilter(src, dst, -1, ksize, anchor, true, borderType);
-}
